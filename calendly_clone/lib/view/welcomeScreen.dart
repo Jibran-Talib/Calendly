969b5c9d90f9b8de189e29218ba445febe242903
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-07-04 23:03:27 +0500

    final

--- calendly_clone/lib/view/welcomeScreen.dart
+++ calendly_clone/lib/view/welcomeScreen.dart
@@ -1,4 +1,6 @@
+import 'package:calendly_clone/utils/reuseable_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -6,8 +8,28 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 45,
+            ),
+            Container(
+              color: Colors.red,
+              child: Image(
+                  width: Get.width * 0.5,
+                  image: const AssetImage('assets/images/longicon.png')),
+            ),
+            const ReuseText(
+              text: 'Connect \nwith Calendly',
+              color: Colors.black87,
+              size: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ],
+        ),
       ),
     );
   }
