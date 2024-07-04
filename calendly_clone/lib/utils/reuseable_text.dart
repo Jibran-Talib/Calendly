550de643c608941d0d30211ea784fb4033c2f775
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-07-04 22:40:37 +0500

    all pub uploaded

---
+++ calendly_clone/lib/utils/reuseable_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ReuseText extends StatelessWidget {
+  final String text;
+  final FontWeight? fontWeight;
+  final double size;
+  final Color color;
+
+  const ReuseText(
+      {super.key,
+      required this.text,
+      this.fontWeight,
+      this.size = 15,
+      this.color = const Color(0xff0047ff)});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Inter',
+        fontWeight: fontWeight,
+        fontSize: size,
+        color: color,
+      ),
+    );
+  }
+}
