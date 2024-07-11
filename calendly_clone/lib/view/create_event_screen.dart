a6bcc54b0c601e804cb84fbf24bf16fede790ff4
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-07-11 20:17:31 +0500

    .

--- calendly_clone/lib/view/create_event_screen.dart
+++ calendly_clone/lib/view/create_event_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class CreateEventScreen extends StatelessWidget {
   const CreateEventScreen({super.key});
@@ -13,47 +14,100 @@ class CreateEventScreen extends StatelessWidget {
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
-          body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 28.h),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 29.h,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 21.w,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: const Icon(
+                          color: Color(0xff757575), Icons.arrow_back_ios)),
+                  const ReuseText(
+                      text: 'Cancel', size: 13, color: Color(0xff757575)),
+                ],
+              ),
+              SizedBox(
+                height: 22.h,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 24.w,
+                  ),
+                  const ReuseText(
+                    text: 'New Event Type',
+                    size: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              SizedBox(
+                // width: double.infinity,
+                child: Row(
                   children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                            color: Color(0xff757575), Icons.arrow_back_ios)),
-                    const ReuseText(
-                        text: 'Cancel', size: 13, color: Color(0xff757575)),
+                    SizedBox(
+                      width: 7.w,
+                    ),
+                    TabBar(
+                        dividerHeight: 0,
+                        splashBorderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        overlayColor:
+                            const MaterialStatePropertyAll(Color(0xffa2cdff)),
+                        // labelPadding: EdgeInsets.only(left: 24.w, bottom: 14),
+
+                        tabAlignment: TabAlignment.center,
+                        indicatorColor: Colors.blue,
+                        labelPadding: EdgeInsets.symmetric(
+                            vertical: 8.h, horizontal: 17.w),
+                        tabs: const [
+                          ReuseText(
+                            text: 'Edit',
+                            size: 13,
+                            color: Color(0xff757575),
+                          ),
+                          ReuseText(
+                            text: 'Preview',
+                            size: 13,
+                            color: Color(0xff757575),
+                          ),
+                        ]),
                   ],
                 ),
-                SizedBox(
-                  height: 22.h,
-                ),
-                const ReuseText(
-                  text: 'New Event Type',
-                  size: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                SizedBox(
-                  height: 25.h,
-                ),
-                const TabBar(indicatorColor: Colors.blue, tabs: [
-                  ReuseText(
-                    text: 'Edit',
-                    size: 13,
-                    color: Color(0xff757575),
+              ),
+              Container(
+                height: 1.h,
+                width: double.infinity,
+                color: const Color(0xffD9D9D9),
+              ),
+              Expanded(
+                child: TabBarView(children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                    ),
                   ),
-                  ReuseText(
-                    text: 'Preview',
-                    size: 13,
-                    color: Color(0xff757575),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                    ),
                   ),
-                ])
-              ],
-            ),
+                ]),
+              )
+            ],
           ),
         ),
       ),
