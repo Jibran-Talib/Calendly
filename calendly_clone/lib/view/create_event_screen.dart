9b5c76d1fc02869fdac4a4d34c818ca52382613b
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-07-11 18:46:08 +0500

    .

--- calendly_clone/lib/view/create_event_screen.dart
+++ calendly_clone/lib/view/create_event_screen.dart
@@ -10,53 +10,50 @@ class CreateEventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 28.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                          color: Color(0xff757575), Icons.arrow_back_ios)),
-                  const ReuseText(
-                      text: 'Cancel', size: 13, color: Color(0xff757575)),
-                ],
-              ),
-              SizedBox(
-                height: 22.h,
-              ),
-              const ReuseText(
-                text: 'New Event Type',
-                size: 24,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-              SizedBox(
-                height: 25.h,
-              ),
-              const DefaultTabController(
-                  length: 2,
-                  child: Column(
-                    children: [
-                      TabBar(indicatorColor: Colors.blue, tabs: [
-                        ReuseText(
-                          text: 'Edit',
-                          size: 13,
-                          color: Color(0xff757575),
-                        ),
-                        ReuseText(
-                          text: 'Preview',
-                          size: 13,
-                          color: Color(0xff757575),
-                        ),
-                      ]),
-                    ],
-                  ))
-            ],
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 28.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                            color: Color(0xff757575), Icons.arrow_back_ios)),
+                    const ReuseText(
+                        text: 'Cancel', size: 13, color: Color(0xff757575)),
+                  ],
+                ),
+                SizedBox(
+                  height: 22.h,
+                ),
+                const ReuseText(
+                  text: 'New Event Type',
+                  size: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                SizedBox(
+                  height: 25.h,
+                ),
+                const TabBar(indicatorColor: Colors.blue, tabs: [
+                  ReuseText(
+                    text: 'Edit',
+                    size: 13,
+                    color: Color(0xff757575),
+                  ),
+                  ReuseText(
+                    text: 'Preview',
+                    size: 13,
+                    color: Color(0xff757575),
+                  ),
+                ])
+              ],
+            ),
           ),
         ),
       ),
