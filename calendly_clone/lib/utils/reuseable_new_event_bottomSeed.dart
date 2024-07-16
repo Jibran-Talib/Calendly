4f1ba99894ab718835afff515de973cbfb0ef824
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-07-16 20:42:05 +0500

    new chnages

---
+++ calendly_clone/lib/utils/reuseable_new_event_bottomSeed.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Reuse extends StatelessWidget {
+  const Reuse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+import 'package:flutter/material.dart';
+
+class Reuse extends StatelessWidget {
+  const Reuse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
