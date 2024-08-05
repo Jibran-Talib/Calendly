9ef9dea8bdf14ed2d806ae6bbfd82022068cebcb
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-08-05 20:59:05 +0500

    calender done

--- calendly_clone/lib/main.dart
+++ calendly_clone/lib/main.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-DateTime get _now => DateTime.now();
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -23,85 +21,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(360, 640),
-      child: CalendarControllerProvider(
-        controller: EventController()..addAll(_events),
-        child: GetMaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            switchTheme: SwitchThemeData(
-              trackOutlineColor: MaterialStateProperty.all(Colors.white),
-            ),
-            iconTheme: const IconThemeData(color: Color(0xff0047ff)),
-            scaffoldBackgroundColor: Colors.white,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-            useMaterial3: true,
+      child: GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          switchTheme: SwitchThemeData(
+            trackOutlineColor: MaterialStateProperty.all(Colors.white),
           ),
-          home: OneOffMeeting(),
+          iconTheme: const IconThemeData(color: Color(0xff0047ff)),
+          scaffoldBackgroundColor: Colors.white,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          useMaterial3: true,
         ),
+        home: OneOffMeeting(),
       ),
     );
   }
 }
-
-List<CalendarEventData> _events = [
-  CalendarEventData(
-    date: _now,
-    title: "Project meeting",
-    description: "Today is project meeting.",
-    startTime: DateTime(_now.year, _now.month, _now.day, 18, 30),
-    endTime: DateTime(_now.year, _now.month, _now.day, 22),
-  ),
-  CalendarEventData(
-    date: _now.add(Duration(days: 1)),
-    startTime: DateTime(_now.year, _now.month, _now.day, 18),
-    endTime: DateTime(_now.year, _now.month, _now.day, 19),
-    title: "Wedding anniversary",
-    description: "Attend uncle's wedding anniversary.",
-  ),
-  CalendarEventData(
-    date: _now,
-    startTime: DateTime(_now.year, _now.month, _now.day, 14),
-    endTime: DateTime(_now.year, _now.month, _now.day, 17),
-    title: "Football Tournament",
-    description: "Go to football tournament.",
-  ),
-  CalendarEventData(
-    date: _now.add(Duration(days: 3)),
-    startTime: DateTime(_now.add(Duration(days: 3)).year,
-        _now.add(Duration(days: 3)).month, _now.add(Duration(days: 3)).day, 10),
-    endTime: DateTime(_now.add(Duration(days: 3)).year,
-        _now.add(Duration(days: 3)).month, _now.add(Duration(days: 3)).day, 14),
-    title: "Sprint Meeting.",
-    description: "Last day of project submission for last year.",
-  ),
-  CalendarEventData(
-    date: _now.subtract(Duration(days: 2)),
-    startTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        14),
-    endTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        16),
-    title: "Team Meeting",
-    description: "Team Meeting",
-  ),
-  CalendarEventData(
-    date: _now.subtract(Duration(days: 2)),
-    startTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        10),
-    endTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        12),
-    title: "Chemistry Viva",
-    description: "Today is Joe's birthday.",
-  ),
-];
--- calendly_clone/lib/view/one_off_meeting.dart
+++ calendly_clone/lib/view/one_off_meeting.dart
@@ -1,7 +1,11 @@
+import 'package:calendar_view/calendar_view.dart';
+
 import 'package:calendly_clone/utils/reuseable_button.dart';
 import 'package:calendly_clone/utils/reuseable_text.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,160 +17,228 @@ class OneOffMeeting extends StatefulWidget {
 }
 
 class _OneOffMeetingState extends State<OneOffMeeting> {
-  // List<Meeting> _getDataSource() {
-  //   final List<Meeting> meetings = <Meeting>[];
-  //   final DateTime today = DateTime.now();
-  //   final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
-  //   final DateTime endTime = startTime.add(const Duration(hours: 2));
-  //   meetings.add(Meeting(
-  //       'Conference', startTime, endTime, const Color(0xFF0F8644), false));
-  //   return meetings;
-  // }
+  List<CalendarEventData> _events = [
+    CalendarEventData(
+      date: DateTime.now(),
+      title: "Project meeting",
+      description: "Today is project meeting.",
+      startTime: DateTime(DateTime.now().year, DateTime.now().month,
+          DateTime.now().day, 18, 30),
+      endTime: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 22),
+    ),
+    CalendarEventData(
+      date: DateTime.now().add(Duration(days: 1)),
+      startTime: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 18),
+      endTime: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 19),
+      title: "Wedding anniversary",
+      description: "Attend uncle's wedding anniversary.",
+    ),
+    CalendarEventData(
+      date: DateTime.now(),
+      startTime: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 14),
+      endTime: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 17),
+      title: "Football Tournament",
+      description: "Go to football tournament.",
+    ),
+    CalendarEventData(
+      date: DateTime.now().add(Duration(days: 3)),
+      startTime: DateTime(
+          DateTime.now().add(Duration(days: 3)).year,
+          DateTime.now().add(Duration(days: 3)).month,
+          DateTime.now().add(Duration(days: 3)).day,
+          10),
+      endTime: DateTime(
+          DateTime.now().add(Duration(days: 3)).year,
+          DateTime.now().add(Duration(days: 3)).month,
+          DateTime.now().add(Duration(days: 3)).day,
+          14),
+      title: "Sprint Meeting.",
+      description: "Last day of project submission for last year.",
+    ),
+    CalendarEventData(
+      date: DateTime.now().subtract(Duration(days: 2)),
+      startTime: DateTime(
+          DateTime.now().subtract(Duration(days: 2)).year,
+          DateTime.now().subtract(Duration(days: 2)).month,
+          DateTime.now().subtract(Duration(days: 2)).day,
+          14),
+      endTime: DateTime(
+          DateTime.now().subtract(Duration(days: 2)).year,
+          DateTime.now().subtract(Duration(days: 2)).month,
+          DateTime.now().subtract(Duration(days: 2)).day,
+          16),
+      title: "Team Meeting",
+      description: "Team Meeting",
+    ),
+    CalendarEventData(
+      date: DateTime.now().subtract(Duration(days: 2)),
+      startTime: DateTime(
+          DateTime.now().subtract(Duration(days: 2)).year,
+          DateTime.now().subtract(Duration(days: 2)).month,
+          DateTime.now().subtract(Duration(days: 2)).day,
+          10),
+      endTime: DateTime(
+          DateTime.now().subtract(Duration(days: 2)).year,
+          DateTime.now().subtract(Duration(days: 2)).month,
+          DateTime.now().subtract(Duration(days: 2)).day,
+          12),
+      title: "Chemistry Viva",
+      description: "Today is Joe's birthday.",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          bottom: PreferredSize(
-            preferredSize: Size(double.infinity, 1.h),
-            child: Material(
-              elevation: 1,
-              child: Container(
-                width: double.infinity,
-                height: 1,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          title: const ReuseText(
-            text: 'New one-off meting',
-            color: Colors.black,
-            size: 16,
-            fontWeight: FontWeight.bold,
-          ),
-          actions: [
-            const Icon(
-              Icons.close,
-            ),
-            SizedBox(
-              width: 15.w,
-            ),
-          ],
-        ),
-        bottomNavigationBar: SizedBox(
-          height: 50.h,
-          child: Row(children: [
-            SizedBox(
-              width: 91.w,
-            ),
-            InkWell(
-                onTap: () {},
-                child: ReuseText(
-                  text: 'Select times to share',
-                  color: const Color(0xff757575),
-                  size: 12.sp,
-                )),
-            SizedBox(
-              width: 18.w,
-            ),
-            SizedBox(
-                width: 100.w,
-                height: 33.h,
-                child: const ReuseButton(
-                    widget: ReuseText(
-                  text: 'Next',
-                  color: Colors.white,
-                  size: 13,
+            length: 2,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                bottom: PreferredSize(
+                  preferredSize: Size(double.infinity, 1.h),
+                  child: Material(
+                    elevation: 1,
+                    child: Container(
+                      width: double.infinity,
+                      height: 1,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                title: const ReuseText(
+                  text: 'New one-off meting',
+                  color: Colors.black,
+                  size: 16,
                   fontWeight: FontWeight.bold,
-                )))
-          ]),
-        ),
-
-        // Column(
-        //   crossAxisAlignment: CrossAxisAlignment.center, children: [
-        //   SizedBox(
-        //     height: 15.h,
-        //   ),
-        //   Center(
-        //     child: Container(
-        //       width: 251.w,
-        //       height: 45,
-        //       decoration: BoxDecoration(
-        //           color: Colors.grey.shade200,
-        //           borderRadius: BorderRadius.circular(10.r)),
-        //       child: Padding(
-        //         padding: EdgeInsets.all(3.h),
-        //         child: TabBar(
-        //             dividerHeight: 0,
-        //             tabAlignment: TabAlignment.center,
-        //             indicatorSize: TabBarIndicatorSize.tab,
-        //             indicator: BoxDecoration(
-        //               color: Colors.white,
-        //               borderRadius: BorderRadius.circular(7.r),
-        //             ),
-        //             tabs: [
-        //               Row(
-        //                 children: [
-        //                   Icon(
-        //                     Icons.calendar_today_outlined,
-        //                     color: Colors.black,
-        //                     size: 12.h,
-        //                   ),
-        //                   SizedBox(
-        //                     width: 6.w,
-        //                   ),
-        //                   const ReuseText(
-        //                     text: 'Calendar',
-        //                     color: Colors.black,
-        //                     size: 12,
-        //                   ),
-        //                 ],
-        //               ),
-        //               Row(
-        //                 children: [
-        //                   Icon(
-        //                     Icons.format_list_bulleted,
-        //                     color: Colors.black,
-        //                     size: 12.h,
-        //                   ),
-        //                   SizedBox(
-        //                     width: 6.w,
-        //                   ),
-        //                   const ReuseText(
-        //                     text: 'Meeting details',
-        //                     color: Colors.black,
-        //                     size: 12,
-        //                   ),
-        //                 ],
-        //               ),
-        //             ]),
-        //       ),
-        //     ),
-        //   ),
-        //   SizedBox(
-        //     height: 10.h,
-        //   ),
-        //   Divider(
-        //     color: Colors.grey.shade200,
-        //   ),
-        //   // body: SfCalendar(
-        //   //   view: CalendarView.week,
-
-        //   //   dataSource: MeetingDataSource(_getDataSource()),
-        //   //   // by default the month appointment display mode set as Indicator, we can
-        //   //   // change the display mode as appointment using the appointment display
-        //   //   // mode property
-        //   //   monthViewSettings: const MonthViewSettings(
-        //   //       appointmentDisplayMode:
-        //   //           MonthAppointmentDisplayMode.appointment),
-        //   // ),
-        // ]),
-      ),
-    ));
+                ),
+                actions: [
+                  const Icon(
+                    Icons.close,
+                  ),
+                  SizedBox(
+                    width: 15.w,
+                  ),
+                ],
+              ),
+              bottomNavigationBar: SizedBox(
+                height: 50.h,
+                child: Row(children: [
+                  SizedBox(
+                    width: 91.w,
+                  ),
+                  InkWell(
+                      onTap: () {},
+                      child: ReuseText(
+                        text: 'Select times to share',
+                        color: const Color(0xff757575),
+                        size: 12.sp,
+                      )),
+                  SizedBox(
+                    width: 18.w,
+                  ),
+                  SizedBox(
+                      width: 100.w,
+                      height: 33.h,
+                      child: const ReuseButton(
+                          widget: ReuseText(
+                        text: 'Next',
+                        color: Colors.white,
+                        size: 13,
+                        fontWeight: FontWeight.bold,
+                      )))
+                ]),
+              ),
+              body: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 15.h,
+                    ),
+                    Center(
+                      child: Container(
+                        width: 251.w,
+                        height: 45,
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10.r)),
+                        child: Padding(
+                          padding: EdgeInsets.all(3.h),
+                          child: TabBar(
+                              dividerHeight: 0,
+                              tabAlignment: TabAlignment.center,
+                              indicatorSize: TabBarIndicatorSize.tab,
+                              indicator: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(7.r),
+                              ),
+                              tabs: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.calendar_today_outlined,
+                                      color: Colors.black,
+                                      size: 12.h,
+                                    ),
+                                    SizedBox(
+                                      width: 6.w,
+                                    ),
+                                    const ReuseText(
+                                      text: 'Calendar',
+                                      color: Colors.black,
+                                      size: 12,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.format_list_bulleted,
+                                      color: Colors.black,
+                                      size: 12.h,
+                                    ),
+                                    SizedBox(
+                                      width: 6.w,
+                                    ),
+                                    const ReuseText(
+                                      text: 'Meeting details',
+                                      color: Colors.black,
+                                      size: 12,
+                                    ),
+                                  ],
+                                ),
+                              ]),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Divider(
+                      color: Colors.grey.shade200,
+                      endIndent: 0,
+                      indent: 0,
+                      height: 0,
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        children: [
+                          DayView(
+                            controller: EventController()..addAll(_events),
+                          ),
+                          Container(
+                            color: Colors.red.shade50,
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+            )));
   }
 }
 
