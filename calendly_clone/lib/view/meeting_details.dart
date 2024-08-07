31d4a19077da2662dfc402af174aa0cc2aaaa466
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-08-07 22:24:41 +0500

    work end

--- calendly_clone/lib/view/meeting_details.dart
+++ calendly_clone/lib/view/meeting_details.dart
@@ -1,23 +1,36 @@
 import 'package:calendly_clone/utils/reuseable_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MeetingDetails extends StatelessWidget {
+class MeetingDetails extends StatefulWidget {
   const MeetingDetails({super.key});
 
+  @override
+  State<MeetingDetails> createState() => _MeetingDetailsState();
+}
+
+class _MeetingDetailsState extends State<MeetingDetails> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(22.w),
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ReuseText(
-              text: 'Time zone',
-              size: 10,
-              fontWeight: FontWeight.bold,
-              color: Colors.black)
+          InkWell(
+            onTap: () async {
+              final String currentTimeZone =
+                  await FlutterNativeTimezone.getLocalTimezone();
+              print(currentTimeZone);
+            },
+            child: ReuseText(
+                text: 'Time zone',
+                size: 10,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
         ],
       ),
     );
--- calendly_clone/macos/Flutter/GeneratedPluginRegistrant.swift
+++ calendly_clone/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import flutter_native_timezone
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
 }
