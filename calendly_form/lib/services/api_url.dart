9e01aec7c53431f85bada22aedadd5966c01f3d8
Jibran-Talib <108657620+Jibran-Talib@users.noreply.github.com>
2024-09-08 19:27:06 +0500

    this project has been completed

--- calendly_form/lib/services/api_url.dart
+++ calendly_form/lib/services/api_url.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/foundation.dart';
+
 class ApiUrls {
-  static String scheduleCreate = 'http://localhost:8000/schedules/create';
+  static String scheduleCreate =
+      // defaultTargetPlatform == TargetPlatform.android ||
+      //         defaultTargetPlatform == TargetPlatform.iOS
+      //     ? 'http://localhost:8000/schedules/create'
+      //     :
+      'http://10.0.2.2:8000/schedules/create';
 }
--- calendly_form/lib/view/form_screen.dart
+++ calendly_form/lib/view/form_screen.dart
@@ -4,7 +4,9 @@ import 'package:calendly_form/services/api_url.dart';
 import 'package:calendly_form/widgets/reuse_snakbar.dart';
 import 'package:calendly_form/widgets/reuseable_button.dart';
 import 'package:calendly_form/widgets/reuseable_textformField.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -25,102 +27,143 @@ class _FormScreenState extends State<FormScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
     DateTime _selectedDate = DateTime(picked.year, picked.month, picked.day);
     var _selectedTime = picked.toString().split(' ');
 
     print("_selectedDate : $_selectedDate");
     print("_selectedTime : $_selectedTime");
     return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'Meeting Details',
-            style: TextStyle(color: Color(0xff757575)),
-          )),
-      body: Padding(
-        padding: EdgeInsets.all(12.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Form(
-                key: _formkey,
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: _size.width >= 700 ? 120.w : 12.w,
+              vertical: _size.height >= 700 ? 100.w : 12.h),
+          child: Material(
+            borderRadius: BorderRadius.circular(5.w),
+            color: Colors.white,
+            elevation: 3,
+            child: Padding(
+              padding: EdgeInsets.all(10.w),
+              child: SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ReuseTextFormField(
-                      enabledBorderColor: const Color(0xff757575),
-                      focusedBorderColor: const Color(0xff0047ff),
-                      hintText: 'full name',
-                      textEditingController: fullNameEditingController,
+                    Padding(
+                      padding: EdgeInsets.all(8.w),
+                      child: const Text(
+                        'Meeting Details',
+                        style:
+                            TextStyle(color: Color(0xff757575), fontSize: 17),
+                      ),
                     ),
                     SizedBox(
-                      height: 15.h,
+                      height: 10.h,
                     ),
-                    ReuseTextFormField(
-                      enabledBorderColor: const Color(0xff757575),
-                      focusedBorderColor: const Color(0xff0047ff),
-                      hintText: 'email',
-                      textEditingController: emailEditingController,
-                    ),
-                    SizedBox(
-                      height: 15.h,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          width: 160.w,
-                          child: TextField(
-                            onTap: () {
-                              setState(() async {
-                                await _datepicker();
-                                setState(() {});
-                              });
-                            },
-                            readOnly: true,
-                            decoration: InputDecoration(
-                                hintText:
-                                    _selectedTime.toString().substring(0, 10),
-                                prefixIcon: const Icon(Icons.calendar_today),
-                                filled: true,
-                                helperStyle:
-                                    const TextStyle(color: Color(0xff757575)),
-                                helperText: 'Set date',
-                                focusedBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide.none),
-                                enabledBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide.none)),
+                    Form(
+                      key: _formkey,
+                      child: Column(
+                        children: [
+                          ReuseTextFormField(
+                            enabledBorderColor: const Color(0xff757575),
+                            focusedBorderColor: const Color(0xff0047ff),
+                            hintText: 'full name',
+                            textEditingController: fullNameEditingController,
                           ),
-                        ),
-                        SizedBox(
-                          width: 160.w,
-                          child: TextField(
-                            onTap: () async {
-                              await _timePicker();
-                              setState(() {});
-                            },
-                            readOnly: true,
-                            decoration: InputDecoration(
-                                hintText: '${time.hour}:${time.minute}',
-                                prefixIcon: const Icon(Icons.access_time),
-                                filled: true,
-                                helperStyle:
-                                    const TextStyle(color: Color(0xff757575)),
-                                helperText: 'set Time',
-                                focusedBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide.none),
-                                enabledBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide.none)),
+                          SizedBox(
+                            height: 15.h,
                           ),
-                        ),
-                      ],
+                          ReuseTextFormField(
+                            enabledBorderColor: const Color(0xff757575),
+                            focusedBorderColor: const Color(0xff0047ff),
+                            hintText: 'email',
+                            textEditingController: emailEditingController,
+                          ),
+                          SizedBox(
+                            height: 15.h,
+                          ),
+                          SizedBox(
+                            width: 400.w,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  width: _size.width >= 700 ? 48.w : 150.w,
+                                  child: TextField(
+                                    onTap: () {
+                                      setState(() async {
+                                        await _datepicker();
+                                        setState(() {});
+                                      });
+                                    },
+                                    readOnly: true,
+                                    decoration: InputDecoration(
+                                        hintText: _selectedTime
+                                            .toString()
+                                            .substring(1, 11),
+                                        prefixIcon:
+                                            const Icon(Icons.calendar_today),
+                                        filled: true,
+                                        helperStyle: const TextStyle(
+                                            color: Color(0xff757575)),
+                                        helperText: 'Set date',
+                                        focusedBorder: const OutlineInputBorder(
+                                            borderSide: BorderSide.none),
+                                        enabledBorder: const OutlineInputBorder(
+                                            borderSide: BorderSide.none)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: _size.width >= 700 ? 48.w : 150.w,
+                                  child: TextField(
+                                    onTap: () async {
+                                      await _timePicker();
+                                      setState(() {});
+                                    },
+                                    readOnly: true,
+                                    decoration: InputDecoration(
+                                        errorBorder: OutlineInputBorder(
+                                          borderSide: const BorderSide(
+                                              color: Colors
+                                                  .red), // Error border color
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        hintText: '${time.hour}:${time.minute}',
+                                        prefixIcon:
+                                            const Icon(Icons.access_time),
+                                        filled: true,
+                                        helperStyle: const TextStyle(
+                                            color: Color(0xff757575)),
+                                        helperText: 'set Time',
+                                        focusedBorder: const OutlineInputBorder(
+                                            borderSide: BorderSide.none),
+                                        enabledBorder: const OutlineInputBorder(
+                                            borderSide: BorderSide.none)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                     SizedBox(
                       height: 70.h,
                     ),
                     InkWell(
                       onTap: () async {
-                        await postApiFunc();
+                        var body = {
+                          'fullname': fullNameEditingController.text,
+                          'email': emailEditingController.text,
+                          'schedule_dt': '$picked ${time.hour}:${time.minute}',
+                        };
+                        print('body : $body');
+                        if (_formkey.currentState!.validate()) {
+                          await postApiFunc(body);
+                        }
                       },
                       child: const ReuseButton(
                           widget: Text(
@@ -130,8 +173,10 @@ class _FormScreenState extends State<FormScreen> {
                       )),
                     )
                   ],
-                ))
-          ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
@@ -168,24 +213,20 @@ class _FormScreenState extends State<FormScreen> {
     }
   }
 
-  Future<void> postApiFunc() async {
+  Future<void> postApiFunc(body) async {
+    print('postApiFunc running');
     try {
-      var body = jsonEncode({
-        'fullname': fullNameEditingController.text,
-        'email': emailEditingController.text,
-        'schedule_dt': '$picked ${time.hour}:${time.minute}',
-        'message': '',
-      });
-      var response = await http.post(Uri.parse(ApiUrls.scheduleCreate),
+      var response = await http.post(Uri.parse(await ApiUrls.scheduleCreate),
           headers: {
-            "Access-Control-Allow-Origin: *"
-                'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-          body: body);
+          body: jsonEncode(body));
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
         ReuseSnakbar().snakbar(response.body);
+        print('postApiFunc ending');
       } else {
+        print('postApiFunc ending');
         ReuseSnakbar().snakbar(response.body + response.statusCode.toString());
       }
     } catch (e) {
